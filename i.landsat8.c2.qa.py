060bf8c4753e397ff89c03588b4da34f084e3107
Nikos Alexandris <nik@nikosalexandris.net>
2021-06-24 00:21:02 +0300

    Minor indentation fix

--- i.landsat8.c2.qa.py
+++ i.landsat8.c2.qa.py
@@ -385,7 +385,7 @@ def main():
 
     # Print to stdout if no output file is specified
     if not output:
-            sys.stdout.write(reclassification_rules)
+        sys.stdout.write(reclassification_rules)
 
     else:
         with open(output, 'w') as o:
